2af5848618bedac923d9903a79d58f54ae333ee8
Luis Najarro <luisnajarro20@gmail.com>
2024-07-11 11:34:09 -0600

    Working with Multiple Slices

--- 19-react-redux-basics/src/App.jsx
+++ 19-react-redux-basics/src/App.jsx
@@ -1,7 +1,15 @@
 import Counter from './components/Counter.jsx';
+import Header from './components/Header.jsx';
+import Auth from './components/Auth.jsx';
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <Header />
+      <Auth />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
