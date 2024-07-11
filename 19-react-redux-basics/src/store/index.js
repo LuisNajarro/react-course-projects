5fcf4e6e9956061e719348c76de5b0ea5a849081
Luis Najarro <luisnajarro20@gmail.com>
2024-07-11 11:09:53 -0600

    Adding State Slices

--- 19-react-redux-basics/src/store/index.js
+++ 19-react-redux-basics/src/store/index.js
@@ -1,8 +1,28 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initalState = { counter: 0, showCounter: true };
+const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initalState, action) => {
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
