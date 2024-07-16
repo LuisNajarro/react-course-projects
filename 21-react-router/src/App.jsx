18b83a467c1c21c392d356cff2e736ce744e4a81
Luis Najarro <luisnajarro20@gmail.com>
2024-07-16 11:02:13 -0600

    Defining and Using Dynamic Routes

--- 21-react-router/src/App.jsx
+++ 21-react-router/src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from './pages/Home.jsx';
 import ProductsPage from './pages/Products.jsx';
 import RootLayout from './pages/Root.jsx';
 import ErrorPage from './pages/Error.jsx';
+import ProductDetailPage from './pages/ProductDetail.jsx';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
     ],
   },
 ]);
