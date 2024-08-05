edd5659ae9afec98948d0700ceabd4cc05c1e74b
Luis Najarro <luisnajarro20@gmail.com>
2024-08-05 17:20:25 -0600

    Changing Data with Mutations

--- 24-react-query/frontend-events/src/util/http.js
+++ 24-react-query/frontend-events/src/util/http.js
@@ -19,3 +19,24 @@ export async function fetchEvents({ signal, searchTerm }) {
 
   return events;
 }
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`https://localhost:7290/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
